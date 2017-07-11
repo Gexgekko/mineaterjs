1560c096b2ed330c1b9947a4fc2b3e9b2dfa5d68
Gexgekko <scgexx@gmail.com>
2017-07-11 22:49:11 +0200

    Almost fixed board

--- classes/Board.js
+++ classes/Board.js
@@ -32,36 +32,45 @@ $.extend(Board.prototype, {
 		return this.cells[position[0]][position[1]];
 	},
 	createPath: function(start, end) {
-		if(start[1] <= end[1]) {
+		if(start[1] < end[1]) {
 			var point = [start[0], end[1]];
 			this.printCell(point, new Cell(AT, true, point));
 			for(var i = start[1] + 1; i <= end[1]; i++) {
 				this.printCell([start[0], i], new Cell(ROCK_FLOOR, true, [start[0], i]));
-				console.log("If1 Printing " + start[0] + "," + i);
 			}
+			var vari = 1;
 
 		} else {
 			var point = [end[0], start[1]];
 			this.printCell(point, new Cell(AT, true, point));
 			for(var i = end[1] + 1; i <= start[1]; i++) {
 				this.printCell([end[0], i], new Cell(ROCK_FLOOR, true, [end[0], i]));
-				console.log("Else1 Printing " + end[0] + "," + i);
 			}
-		}
-
-		if(start[0] <= end[0]) {
-			for(var i = point[0] + 1; i < end[0]; i++) {
-				this.printCell([i, point[1]], new Cell(ROCK_FLOOR, true, [i, point[1]]));
-				console.log("If2 Printing " + i + "," + point[1]);
-			}
-		} else {
-			for(var i = end[0] + 1; i < point[0]; i++) {
-				this.printCell([i, end[1]], new Cell(ROCK_FLOOR, true, [i, end[1]]));
-				console.log("Else2 Printing " + i + "," + end[1]);
-			}
+			var vari = 0;
 		}
 		
-		//this.printCell(point, new Cell(AT, true, points[i]));
+		if(start[0] <= end[0]) {
+			if(vari == 0) {
+				for(var i = start[0] + 1; i < point[0]; i++) {
+					this.printCell([i, point[1]], new Cell(ROCK_FLOOR, true, [i, point[1]]));
+				}
+			} else {
+				for(var i = point[0] + 1; i < end[0]; i++) {
+					this.printCell([i, point[1]], new Cell(ROCK_FLOOR, true, [i, point[1]]));
+				}
+			}
+			
+		} else {
+			if(vari == 0) {
+				for(var i = start[0] - 1; i > point[0]; i--) {
+					this.printCell([i, point[1]], new Cell(ROCK_FLOOR, true, [i, point[1]]));
+				}
+			} else {
+				for(var i = point[0] - 1; i > end[0]; i--) {
+					this.printCell([i, point[1]], new Cell(ROCK_FLOOR, true, [i, point[1]]));
+				}
+			}
+		}
 		
 	}
 });
